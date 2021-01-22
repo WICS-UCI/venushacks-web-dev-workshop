561c69ab07b57904492ce94475b5adb82e6d3699
Gayatri Subramaniam <gayatrs@uci.edu>
2021-01-22 01:22:41 -0800

    Add comments to main.py.

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+# Python Library Import
+
+#Flask App
 from flask import Flask, request
+# JSON Parsing
 import json
-import time
+# MondoDB 
 import pymongo
+# Mongo Object  
 from bson import json_util
 
+# Creating Flask App object
 app = Flask(__name__,
            static_url_path='',
            static_folder='static')
 
+# Each method in the Flask app is tied to a url, which is wriiten using '@app.route()'
 @app.route('/')
 def root():
+    # Default an HTML page
     return app.send_static_file('index.html')
 
+# Regular GET method (doesn't receive information, sends back information)
 @app.route('/view')
 def viewItems():
     client = pymongo.MongoClient("mongodb+srv://gayatrs:Bridge1!@cluster0.u03w3.mongodb.net/Netflix?retryWrites=true&w=majority")
@@ -20,6 +29,7 @@ def viewItems():
     log = list(col.find())
     return json_util.dumps(log)
 
+# POST method (receives information, sends back information)
 @app.route('/add', methods=['POST'])
 def addItems():
     client = pymongo.MongoClient("mongodb+srv://gayatrs:Bridge1!@cluster0.u03w3.mongodb.net/Netflix?retryWrites=true&w=majority")
@@ -28,5 +38,6 @@ def addItems():
     col.insert_one({'name': request.form['name'], 'time': request.form['time'], 'genre': request.form['genre']})
     return app.send_static_file('view.html')
 
+# Runs Flask App
 if __name__ == "__main__":
     app.run()
