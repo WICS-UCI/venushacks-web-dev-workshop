7e1075c0a9ac6889c5c86f81fcffc3acb82540ab
Gayatri Subramaniam <gayatrs@uci.edu>
2021-01-22 22:08:50 -0800

    Add TODO comments.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Python Library Import
+# Python Library Imports
 
 #Flask App
 from flask import Flask, request
@@ -23,20 +23,14 @@ def root():
 # Regular GET method (doesn't receive information, sends back information)
 @app.route('/view')
 def viewItems():
-    client = pymongo.MongoClient("mongodb+srv://gayatrs:Bridge1!@cluster0.u03w3.mongodb.net/Netflix?retryWrites=true&w=majority")
-    db = client["Netflix"]
-    col = db['Logs']
-    log = list(col.find())
-    return json_util.dumps(log)
+    # TO-DO #1
+    pass
 
 # POST method (receives information, sends back information)
 @app.route('/add', methods=['POST'])
 def addItems():
-    client = pymongo.MongoClient("mongodb+srv://gayatrs:Bridge1!@cluster0.u03w3.mongodb.net/Netflix?retryWrites=true&w=majority")
-    db = client["Netflix"]
-    col = db['Logs']
-    col.insert_one({'name': request.form['name'], 'time': request.form['time'], 'genre': request.form['genre']})
-    return app.send_static_file('view.html')
+    # TO-DO #2
+    pass
 
 # Runs Flask App
 if __name__ == "__main__":
