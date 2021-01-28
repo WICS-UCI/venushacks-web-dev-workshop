11ccdc783421083d6edcca321597c4d620b4e54e
Gayatri Subramaniam <gayatrs@uci.edu>
2021-01-27 16:01:48 -0800

    Use .env

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pymongo
 # Mongo Object  
 from bson import json_util
 # ENV Import
-from dotenv import load_dotenv, find_dotenv
+import os
 
 # Creating Flask App object
 app = Flask(__name__,
@@ -25,7 +25,8 @@ def root():
 # Regular GET method (doesn't receive information, sends back information)
 @app.route('/view')
 def viewItems():
-    client = pymongo.MongoClient("mongodb+srv://gayatrs:Bridge1!@cluster0.u03w3.mongodb.net/Netflix?retryWrites=true&w=majority")
+    mongourl = os.getenv('MONGOURL')
+    client = pymongo.MongoClient(mongourl)
     db = client["Netflix"]
     col = db['Logs']
     log = list(col.find())
@@ -34,7 +35,8 @@ def viewItems():
 # POST method (receives information, sends back information)
 @app.route('/add', methods=['POST'])
 def addItems():
-    client = pymongo.MongoClient("mongodb+srv://gayatrs:Bridge1!@cluster0.u03w3.mongodb.net/Netflix?retryWrites=true&w=majority")
+    mongourl = os.getenv('MONGOURL')
+    client = pymongo.MongoClient(mongourl)
     db = client["Netflix"]
     col = db['Logs']
     col.insert_one({'name': request.form['name'], 'time': request.form['time'], 'genre': request.form['genre']})
