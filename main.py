09382cda0c7066b45dc7c1af9f8495e31ad410d8
Gayatri Subramaniam <gayatrs@uci.edu>
2021-04-16 15:19:54 -0700

    Fix backend to work with dictionary (update JS as well)

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 # Python Library Import
-
 #Flask App
 from flask import Flask, request
 # JSON Parsing
 import json
-# MondoDB 
-import pymongo
-# Mongo Object  
-from bson import json_util
 # ENV Import
 import os
 
 # Creating Flask App object
 app = Flask(__name__,
            static_url_path='',
-           static_folder='static')
+           static_folder='static')\
+
+issue_tracker = []
 
 # Each method in the Flask app is tied to a url, which is wriiten using '@app.route()'
 @app.route('/')
@@ -25,21 +22,19 @@ def root():
 # Regular GET method (doesn't receive information, sends back information)
 @app.route('/view')
 def viewItems():
-    mongourl = os.getenv('MONGOURL')
-    client = pymongo.MongoClient(mongourl)
-    db = client["Netflix"]
-    col = db['Logs']
-    log = list(col.find())
-    return json_util.dumps(log)
+    return json.dumps(issue_tracker)
+    # mongourl = os.getenv('MONGOURL')
+    # client = pymongo.MongoClient(mongourl)
+    # db = client["Netflix"]
+    # col = db['Logs']
+    # log = list(col.find())
+    # return json_util.dumps(log)
 
 # POST method (receives information, sends back information)
 @app.route('/add', methods=['POST'])
 def addItems():
-    mongourl = os.getenv('MONGOURL')
-    client = pymongo.MongoClient(mongourl)
-    db = client["Netflix"]
-    col = db['Logs']
-    col.insert_one({'name': request.form['name'], 'time': request.form['time'], 'genre': request.form['genre']})
+    item = {'item': request.form['item'], 'amount': request.form['amount'], 'category': request.form['category']}
+    issue_tracker.append(item)
     return app.send_static_file('view.html')
 
 # Runs Flask App
--- static/script.js
+++ static/script.js
@@ -9,10 +9,10 @@ function getResult() {
 
       // For each item in the list that is returned in the Python function, we add the equivalent table HTML string
       // For each loop
-      $.each(JSON.parse(result), function(index, item){
+      $.each(JSON.parse(result), function(index, elem){
         console.log("added");
         // Appending HTML
-        $('#result_table').append('<tr><td>' + item.name + '</td><td>' + item.genre + '</td><td>' + item.time + '</td></tr>');
+        $('#result_table').append('<tr><td>' + elem.item + '</td><td>' + elem.amount + '</td><td>' + elem.category + '</td></tr>');
       });
     }
   });
