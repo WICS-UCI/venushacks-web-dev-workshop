25acf96c8a29da0a98fda1636321ba3e065f8abe
Kasey Chuang <chuangky@uci.edu>
2021-04-21 23:47:29 -0700

    implemented jinja and cleaned up code.

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 # Python Library Import
 #Flask App
-from flask import Flask, request, render_template
-# JSON Parsing
-import json
-# ENV Import
-import os
+from flask import Flask, request, render_template, redirect
 
 # Creating Flask App object
 app = Flask(__name__, template_folder="templates")
 
-issue_tracker = []
-category = 'all'
+expenses = []
 
 # Each method in the Flask app is tied to a url, which is wriiten using '@app.route()'
 @app.route('/')
@@ -18,27 +13,34 @@ def root():
     # Default an HTML page
     return render_template('index.html')
 
-@app.route('/viewcat', methods=['POST'])
-def changeCategory():
-    global category
-    category = request.form['cat']
-    return render_template('view.html', ct=category)
+@app.route('/add-page')
+def add_page():
+    return render_template('add.html')
 
+@app.route('/view-page')
+def view_page():
+    return render_template('view.html', category="all")
+
+@app.route('/view-cat', methods=['POST'])
+def change_category():
+    category = request.form['cat']
+    return redirect('/view/' + category)
+    
 # Regular GET method (doesn't receive information, sends back information)
-@app.route('/view')
-def viewItems():
-    # print(request.form['cat'])
+@app.route('/view/<category>')
+def view(category):
     if category == 'all':
-        return json.dumps(issue_tracker)
+        data= expenses
     else:
-        return json.dumps([i for i in issue_tracker if i['category'] == category])
+        data =  [i for i in expenses if i['category'] == category]
+    return render_template('view.html', category=category, data=data)
 
 # POST method (receives information, sends back information)
 @app.route('/add', methods=['POST'])
-def addItems():
+def add_item():
     item = {'item': request.form['item'], 'amount': request.form['amount'], 'category': request.form['category']}
-    issue_tracker.append(item)
-    return render_template('view.html')
+    expenses.append(item)
+    return redirect('view/all')
 
 # Runs Flask App
 if __name__ == "__main__":
